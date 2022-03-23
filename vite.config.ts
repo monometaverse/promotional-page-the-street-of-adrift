f24462691e030bb3aadae5a0e0b5687801303fdc
LemonNeko <chheese048@gmail.com>
2022-03-23 14:57:59 +0800

    :wrench: 更新配置文件以导入要使用的 3 个字体

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    fonts({
+      google: {
+        families: ['Noto Sans SC', 'Montserrat', 'Noto Serif SC']
+      }
+    })
+  ],
   assetsInclude: [/glb/, /hdr/],
   build: {
     assetsInlineLimit: 0,
