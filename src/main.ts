827234057d17a0dd995e49d86a47799c7e47f6a9
LemonNeko <chheese048@gmail.com>
2022-03-25 17:05:11 +0800

    :wrench: 引入 element 样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {i18n} from './locales'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from '~pages'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const router = createRouter({
   history: createWebHashHistory(),
