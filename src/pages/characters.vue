59a7efee9889543d9577895accc7e1dfea6ca062
LemonNeko <chheese048@gmail.com>
2022-03-30 17:51:18 +0800

    :sparkles: 完成角色页右侧布局

--- src/pages/characters.vue
+++ src/pages/characters.vue
@@ -21,9 +21,45 @@ const currentShow = ref(0)
         </div>
       </div>
     </div>
+    <!-- 角色介绍 -->
+    <div class="info">
+      <div class="info-title">
+        {{ characters[currentShow] }}
+      </div>
+      <div class="info-title-en">
+        SHIRAISHI ROSETTA
+      </div>
+      <div class="info-more">
+        <div class="info-more-item">
+          <div class="info-more-item-title">
+            ILLUSTRATOR
+          </div>
+          <div class="info-more-item-content">
+            yannn
+          </div>
+        </div>
+        <div class="info-more-item">
+          <div class="info-more-item-title">
+            CV
+          </div>
+          <div class="info-more-item-content">
+            范馨源
+          </div>
+        </div>
+      </div>
+      <div class="info-divider" />
+      <div class="info-desc">
+        她是巨型企业白石集团董事长的女儿，出于保护人身安全的目的，她的父母为她创造了一个假名[九条泷川]，并让她在福京市理工大学学习，就读理论物理专业。
+        然而，纵使白石的物质生活极度优越，可她却总是被父母的意志所支配，就连学习的专业与未来的工作都在计划板上被写的清清楚楚。
+        她并不愿意屈服于眼前这已经被安排好的日复一日的生活。她一直以来都有一个梦想，那便是用自己的力量，帮助其他人，为这座快要烂掉的城市做出一份贡献。
+
+        一直以来，她都在寻找这样一个契机。
+      </div>
+    </div>
   </div>
 </template>
 <style lang="less" scoped>
+// 角色选择器
 .selector {
   @top: calc(50% - calc(32px * 8 + 24px * 7) / 2);
 
@@ -76,6 +112,71 @@ const currentShow = ref(0)
     &:hover {
       color: white;
     }
+  }
+}
+// 角色介绍
+.info {
+  position: absolute;
+  right: 375px;
+  bottom: calc(171px + 50vh - 540px);
+
+  &-title {
+    font-size: 64px;
+    line-height: 92px;
+    font-family: 'Noto Serif SC', sans-serif;
+    font-weight: 900;
+  }
+
+  &-title-en {
+    font-size: 24px;
+    line-height: 29px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    opacity: 0.5;
+  }
+
+  &-more {
+    display: flex;
+    margin-top: 24px;
+    column-gap: 24px;
+
+    &-item {
+      &-title {
+        padding-left: 8px;
+        width: 128px;
+        height: 24px;
+        background-color: white;
+        color: #818181;
+        font-family: 'Noto Sans SC', sans-serif;
+        font-size: 14px;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+      }
+
+      &-content {
+        line-height: 32px;
+        font-size: 14px;
+        font-family: 'Noto Sans SC', sans-serif;
+        font-weight: 400;
+      }
+    }
+  }
+
+  &-divider {
+    margin-top: 24px;
+    height: 2px;
+    width: 16px;
+    background-color: white;
+  }
+
+  &-desc {
+    margin-top: 24px;
+    width: 480px;
+    font-size: 12px;
+    font-weight: 400;
+    font-family: 'Noto Sans SC', sans-serif;
+    line-height: 28px;
   }
 }
 </style>
