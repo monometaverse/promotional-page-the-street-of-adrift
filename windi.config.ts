edb0be16711822d377386362594ab8fc457ae985
LemonNeko <chheese048@gmail.com>
2022-04-15 13:11:34 +0800

    :wrench: 更新 windicss 配置

--- windi.config.ts
+++ windi.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'windicss/helpers'
 export default defineConfig({
   content: [
     './src/**/*.vue'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans SC', 'sans-serif'],
+        serif: ['Noto Serif SC', 'sans-serif'],
+        monsterrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
+  }
 })
