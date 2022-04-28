c27a247f0615a6b25f3e534a999dd2bd4de975ee
LemonNeko <chheese048@gmail.com>
2022-04-28 17:57:07 +0800

    :new: 添加用户信息 API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios"
+
 export type SuccessResponse<T> = {
   code: 0,
   message: string,
@@ -19,28 +21,88 @@ export const isSuccess = <T>(res: SuccessResponse<T> | FailureResponse): res is 
   return res.code === 0
 }
 
+/**
+ * 登录信息接口返回体
+ */
+export interface LoginInfo {
+  avatar: string
+  city: string
+  country: string
+  disable: number
+  displayName: string
+  id: string
+  intro: string
+}
+
+// 拦截器拦下来的错误的编号
+export enum interceptorsErrorCode {
+  unknown = -1, // 未知错误
+  timeout = 1,  // 请求超时
+  network       // 网络错误
+}
+
+// 经过处理的 Axios
+interface MonoAxios extends AxiosInstance {
+  get<T, R = MonoResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>
+}
+
+const monoAxios: MonoAxios = (() => {
+  const instance = axios.create({
+    baseURL: 'https://uat.mono.fun/api',
+    withCredentials: true,
+  })
+  // 添加错误拦截器
+  instance.interceptors.response.use((res) => res.data, (error): FailureResponse => {
+    if (!(error instanceof AxiosError)) return {
+      code: interceptorsErrorCode.unknown,
+      message: '未知的错误'
+    }
+    // 收到了表示错误的回应
+    if (error.response) {
+      return {
+        code: error.response.data?.code ?? error.response.status,
+        message: error.response.data?.message ?? '未知错误信息'
+      }
+    }
+    // 没有收到任何回应，因为超时了
+    if (error.message.includes('timeout')) {
+      return {
+        code: interceptorsErrorCode.timeout,
+        message: '请求超时'
+      }
+    }
+    // 没有收到任何回应，因为网络错误
+    if (error.message.includes('Network Error')) {
+      return {
+        code: interceptorsErrorCode.timeout,
+        message: '请求超时'
+      }
+    }
+    return {
+      code: interceptorsErrorCode.unknown,
+      message: '未知的错误'
+    }
+  })
+
+  return instance
+})()
+
 export default {
   user: {
-
+    /**
+     * 获取登录状态
+     * 如果已经登录了，会返回账号信息
+     */
+    getLoginInfo: () => {
+      return monoAxios.get<LoginInfo>('/v1/user/info')
+    },
   },
   nft: {
     /**
      * NFT 预约
-     * TODO: 目前是 mock 逻辑
      */
-    reserve: (nftName: string): MonoResponse<null> => {
-      const isReserved = localStorage.getItem(`NFTReserved:${nftName}`)
-      if (isReserved === 'true') {
-        return {
-          code: 40901,
-          message: '已经预约过了'
-        }
-      }
-      localStorage.setItem(`NFTReserved:${nftName}`, 'true')
-      return {
-        code: 0,
-        message: '预约成功'
-      }
+    reserve: (nftName: string) => {
+      return monoAxios.get<string>('/v1/project/reserve')
     },
     /**
      * 获取 NFT 预约状态
